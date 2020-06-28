a8d28f08d396c49012ae196f87170f9946cce991
Michał Gumiela <michal.gumiela@gmail.com>
2020-06-29 00:06:58 +0200

    radfet 39 - 41 added

--- name_changer.m
+++ name_changer.m
@@ -1,4 +1,4 @@
-radfet = radfet38parsedconverted;
+radfet = radfet41parsedconverted;
 
 radfet.Properties.VariableNames{1} = 'Vdiode';
 radfet.Properties.VariableNames{2} = 'Vth0';
@@ -7,4 +7,4 @@ radfet.Properties.VariableNames{4} = 'Vth2';
 radfet.Properties.VariableNames{5} = 'PT1000';
 radfet.Properties.VariableNames{6} = 'LMT87';
 
-radfet38converted = radfet;
+radfet41converted = radfet;
