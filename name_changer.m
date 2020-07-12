7756487a50bceaf465264441b15b144f34f24f62
Michał Gumiela <michal.gumiela@gmail.com>
2020-07-12 22:53:03 +0200

    radfet 42 results

--- name_changer.m
+++ name_changer.m
@@ -1,4 +1,4 @@
-radfet = radfet41parsedconverted;
+radfet = radfet42parsedconverted;
 
 radfet.Properties.VariableNames{1} = 'Vdiode';
 radfet.Properties.VariableNames{2} = 'Vth0';
@@ -7,4 +7,4 @@ radfet.Properties.VariableNames{4} = 'Vth2';
 radfet.Properties.VariableNames{5} = 'PT1000';
 radfet.Properties.VariableNames{6} = 'LMT87';
 
-radfet41converted = radfet;
+radfet42converted = radfet;
